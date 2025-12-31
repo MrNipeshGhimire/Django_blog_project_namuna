401611470eb45de2fe83ac9ba64d9f50ec73cb26
Nipesh <nipeshincbca2021@gmai.com>
2025-12-31 13:49:21 +0545

    blog create commit

--- blog_project/blog_app/views/main_view.py
+++ blog_project/blog_app/views/main_view.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from ..models import Blogs
+from django.contrib import messages
 
 
 def index_page(request):
@@ -11,4 +13,35 @@ def about_page(request):
 
 @login_required
 def create_blog(request):
+    errors = {}
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        category = request.POST.get('category')
+        image = request.FILES.get('image')
+        description = request.POST.get('description')
+
+        if not title:
+            errors['title'] = "Title is Required"
+        
+        if errors:
+            return render(request,'main/create_blog.html',{'errors':errors,'data':request.POST})
+        
+        try:
+            # try
+            blog = Blogs.objects.create(
+                title=title,
+                category=category,
+                description=description,
+                author = request.user
+            )
+            blog.save()
+            messages.success(request,"Blog posted successfully !! ")
+            return redirect('index')
+
+        except Exception as e:
+            print(f"Error occurs :{e}")
+            return render(request,'main/create_blog.html',{'errors':"Failed to add blog"})
+
+
+
     return render(request,'main/create_blog.html')
